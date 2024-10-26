23e14f689777b9727efe63b81bca5661f7bbc60d
Joydevbdx <contact.joy.me@gmail.com>
2024-10-27 00:05:59 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,26 @@
+<!-- footer.php -->
+
+<footer class="footer">
+    <p>Created by <strong>VNCIT</strong></p>
+</footer>
+
+<!-- CSS for the footer -->
+<style>
+    .footer {
+        background-color: #e0e5ec;
+        padding: 20px;
+        text-align: center;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        box-shadow: 0px -5px 10px #babecc, 0px 5px 10px #ffffff;
+        font-size: 14px;
+        color: #333;
+        border-radius: 10px 10px 0 0;
+    }
+
+    .footer p {
+        margin: 0;
+        color: #333;
+    }
+</style>
